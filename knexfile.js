174cca4701ecf6344829138bea09cae4562388fa
Venirails <veni.rails@gmail.com>
2022-02-10 17:01:51 +0530

    knex

--- knexfile.js
+++ knexfile.js
@@ -2,10 +2,7 @@ const options = process.env.NODE_ENV === 'production' ?
 {
     client: 'pg',
     connection: process.env.DATABASE_URL,
-    useNullAsDefault: true,
-    dialectOptions: {
-    ssl: true
-    }
+    ssl: { rejectUnauthorized: false }
   } :
 
 
