f70fda4dde9436a4fbf9cd2434954ba8cfd03720
Venirails <veni.rails@gmail.com>
2022-02-10 11:23:28 +0530

    folder arrangements

---
+++ routes/index.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+const path = require('path');
+if (NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+  app.use((req, res) => {
+    res.sendFile(path.join(__dirname, '../client/build/index.html'));
+  });
+}
+
+const PORT = process.env.PORT || 8000;
+app.listen(PORT);
+
+module.exports = router;
