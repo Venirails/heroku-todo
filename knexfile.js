24406411e42111db2cb0883afedf643d21d4b0e8
Venirails <veni.rails@gmail.com>
2022-02-10 16:42:58 +0530

    knex

--- knexfile.js
+++ knexfile.js
@@ -1,7 +1,7 @@
 const options = process.env.NODE_ENV === 'production' ?
 {
     client: 'pg',
-    connection: ' postgres://imiyumklzcefcj:0f000c9341da24e88800352dea2d38b7753a6dd55007c7fa590f267feba41cc4@ec2-34-205-46-149.compute-1.amazonaws.com:5432/d1j5k398ak7s5a'
+    connection: process.env.DATABASE_URL
    
   } :
 
